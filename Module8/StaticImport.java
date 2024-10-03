03564416b62c35ede1dcae0a1b07774e08231c91
Manish Bawane <138232895+manishbawane@users.noreply.github.com>
2024-10-03 17:59:23 +0530

    Add files via upload

---
+++ Module8/StaticImport.java
@@ -0,0 +1,20 @@
+package Module8;
+import static java.lang.Math.*;
+public class StaticImport {
+
+	public static void main(String[] args) {
+		System.out.println(pow(7,4));
+		System.out.println(pow(4,8));
+		System.out.println(pow(9,4));
+
+		System.out.println(random());
+		System.out.println(random());
+		
+		System.out.println(round(4.5));
+		System.out.println(round(3.4));
+		System.out.println(round(5.5));
+		
+		System.out.println(random());
+	}
+
+}
